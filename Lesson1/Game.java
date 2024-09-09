77797f1dbbca0bd9babbfde1a49dc6816fcdfb70
shuriking <shura.kravchenko.1992@gmail.com>
2024-09-09 22:33:41 +0600

    Исправил класс Game

--- Lesson1/Game.java
+++ Lesson1/Game.java
@@ -1,22 +1,14 @@
 public class Game {
     public static void main(String[] args) {
-        int secretNumber = 19;
-        int inputNumberPlayer = 34;
+        int secretNumber = 14;
+        int inputNumberPlayer = 4;
 
         do {
-            if ((secretNumber - inputNumberPlayer > 10) || (secretNumber - inputNumberPlayer > 5) &&
-                    secretNumber > inputNumberPlayer) {
-                System.out.println(inputNumberPlayer + " меньше того , что загадал компьютер");
-                inputNumberPlayer += 5;
-            } else if ((inputNumberPlayer - secretNumber > 10) || (inputNumberPlayer - secretNumber > 5) &&
-                    secretNumber < inputNumberPlayer) {
-                System.out.println(inputNumberPlayer + " больше того , что загадал компьютер");
-                inputNumberPlayer -= 5;
-            } else if (secretNumber - inputNumberPlayer <= 5 && secretNumber > inputNumberPlayer) {
-                System.out.println(inputNumberPlayer + " меньше того , что загадал компьютер");
+            if (secretNumber > inputNumberPlayer) {
+                System.out.println(inputNumberPlayer + " меньше того, что загадал компьютер");
                 inputNumberPlayer++;
-            } else if (inputNumberPlayer - secretNumber <= 5 && secretNumber < inputNumberPlayer) {
-                System.out.println(inputNumberPlayer + " больше того , что загадал компьютер");
+            } else if (secretNumber < inputNumberPlayer) {
+                System.out.println(inputNumberPlayer + " больше того, что загадал компьютер");
                 inputNumberPlayer--;
             }
             if (secretNumber == inputNumberPlayer) {
