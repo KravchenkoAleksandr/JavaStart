1a05e99734e569586bfc44cf308dcaeb9b075982
shuriking <shura.kravchenko.1992@gmail.com>
2024-09-10 22:10:06 +0600

    Добавил класс Person

---
+++ Lesson2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    
+    char sex = 'm';
+    String name = "Bob";
+    int height = 162;
+    float weight = 60.5f;
+    int age = 31;
+
+    void go() {
+        System.out.println("go");
+    }
+
+    void sit() {
+        System.out.println("sit");
+    }
+
+    void run() {
+        System.out.println("run");
+    }
+
+    void speak() {
+        System.out.println("speak");
+    }
+
+    void learningJava() {
+        System.out.println("learningJava");
+    }
+}
