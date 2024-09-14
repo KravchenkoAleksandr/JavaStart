e2b21898e1b5521a482da9cab5f2ac77ee8e450f
shuriking <shura.kravchenko.1992@gmail.com>
2024-09-14 14:25:53 +0600

    добавилcp866

--- Lesson2/GuessNumberTest.java
+++ Lesson2/GuessNumberTest.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class GuessNumberTest {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in, "cp866");
         System.out.println("Введите имя первого игрока");
         String namePlayer1 = scanner.nextLine();
         Player player1 = new Player(namePlayer1);
