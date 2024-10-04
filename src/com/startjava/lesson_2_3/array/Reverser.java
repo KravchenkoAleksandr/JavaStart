e6df0c68a94ca2a648f1573cacaa4c036c879ca2
shuriking <shura.kravchenko.1992@gmail.com>
2024-10-04 11:22:44 +0600

    "Исправил задание"

--- src/com/startjava/lesson_2_3/array/Reverser.java
+++ src/com/startjava/lesson_2_3/array/Reverser.java
@@ -4,29 +4,36 @@ import java.util.Arrays;
 
 public class Reverser {
     public static void main(String[] args) {
-        int[] zero = new int[0];
-        printReverse(zero);
+        int[] empty = new int[0];
+        printReverse("До реверса: ", empty);
+        reverseArray(empty);
+        printReverse("После реверса: ", empty);
         int[] numbers = {6, 8, 9, 1};
-        printReverse(numbers);
+        printReverse("До реверса: ", numbers);
+        reverseArray(numbers);
+        printReverse("После реверса: ", numbers);
         int[] numbers2 = {13, 8, 5, 3, 2, 1, 1};
-        printReverse(numbers2);
-        int[] digits = null;
-        printReverse(digits);
+        printReverse("До реверса: ", numbers2);
+        reverseArray(numbers2);
+        printReverse("После реверса: ", numbers2);
+        int[] nullable = null;
+        printReverse("До реверса: ", nullable);
+        reverseArray(nullable);
+        printReverse("После реверса: ", nullable);
     }
 
-    public static void printReverse(int[] array) {
+    public static void printReverse(String s, int[] array) {
         if (array == null || array.length == 0) {
-            System.out.println("Пустой массив или массив со значение null");
             return;
         }
-        System.out.printf("%15s", "До реверса: ");
-        System.out.println(Arrays.toString(array));
-        reverseArray(array);
-        System.out.printf("%15s", "После реверса: ");
+        System.out.printf("%15s", s);
         System.out.println(Arrays.toString(array));
     }
 
     private static void reverseArray(int[] array) {
+        if (array == null || array.length == 0) {
+            return;
+        }
         int len = array.length;
         for (int i = 0; i < len--; i++) {
             int temp = array[i];
