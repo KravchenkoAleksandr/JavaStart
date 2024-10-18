07c02f952b2f0872926ea05b8ef2feb95c1dde21
shuriking <shura.kravchenko.1992@gmail.com>
2024-10-18 17:22:37 +0600

    "Исправил замечания"

--- src/com/startjava/lesson_2_3/array/UniqueNumsFiller.java
+++ src/com/startjava/lesson_2_3/array/UniqueNumsFiller.java
@@ -8,29 +8,29 @@ public class UniqueNumsFiller {
     static Random rnd = new Random();
 
     public static void main(String[] args) {
-        int[][] numbers = {
+        int[][] matrix = {
                 {-10, 20, 23},
                 {-30, 10, 10},
                 {-34, -34, 0},
                 {-1, 2, -3},
                 {5, -8, 2}
         };
-        for (int[] number : numbers) {
-            if (number[0] > number[1]) {
-                System.out.println("Ошибка: левая граница (" + number[1] + ") > правой (" + number[0] + ")\n");
+        for (int[] numbers : matrix) {
+            if (numbers[0] > numbers[1]) {
+                System.out.println("Ошибка: левая граница (" + numbers[1] + ") > правой (" + numbers[0] + ")\n");
                 continue;
             }
-            if (number[2] <= 0) {
-                System.out.println("Ошибка: количество чисел в строке не может быть меньше 1 (" + number[2] + ")\n");
+            if (numbers[2] <= 0) {
+                System.out.println("Ошибка: количество чисел в строке не может быть меньше 1 (" + numbers[2] + ")\n");
                 continue;
             }
-            int[] copyArray = fill(number[0], number[1], number[2]);
-            printArray(copyArray, number[2]);
+            int[] copyArray = fill(numbers[0], numbers[1], numbers[2]);
+            printArray(copyArray, numbers[2]);
         }
     }
 
-    public static int[] fill(int start, int end, int print) {
-        if (start > end || print <= 0) {
+    public static int[] fill(int start, int end, int quantity) {
+        if (start > end || quantity <= 0) {
             return null;
         }
         double len = (Math.abs(start) + end) * 0.75;
@@ -39,16 +39,16 @@ public class UniqueNumsFiller {
             return new int[0];
         }
         for (int i = 0; i < uniqueNums.length; ) {
-            int unique = rnd.nextInt(start, end + 1);
+            int num = rnd.nextInt(start, end + 1);
             boolean isUnique = true;
             for (int j = 0; j <= i; j++) {
-                if (unique == uniqueNums[j]) {
+                if (num == uniqueNums[j]) {
                     isUnique = false;
                     break;
                 }
             }
             if (isUnique) {
-                uniqueNums[i++] = unique;
+                uniqueNums[i++] = num;
             }
         }
         Arrays.sort(uniqueNums);
@@ -57,7 +57,8 @@ public class UniqueNumsFiller {
 
     public static void printArray(int[] array, int print) {
         if (array == null) return;
-        if (array.length == 0) {
+        int len = array.length;
+        if (len == 0) {
             System.out.println("Ошибка: длина массива должна быть больше 0 (" + array.length + ")\n");
             return;
         }
@@ -66,9 +67,8 @@ public class UniqueNumsFiller {
                 System.out.println("\n");
                 print *= 2;
             }
-            System.out.printf("%3d", array[i]);
+            System.out.printf("%3d ", array[i]);
         }
-        System.out.println();
-        System.out.println();
+        System.out.println("\n");
     }
 }
