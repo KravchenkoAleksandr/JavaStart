d372a7156977cff97961b0ca3b5d5e5fa8051446
shuriking <shura.kravchenko.1992@gmail.com>
2024-10-18 17:50:26 +0600

    "Исправил замечания"

--- src/com/startjava/lesson_2_3/array/TypewriterEffect.java
+++ src/com/startjava/lesson_2_3/array/TypewriterEffect.java
@@ -1,9 +1,8 @@
 package com.startjava.lesson_2_3.array;
 
-public class TypewriterStrings {
+public class TypewriterEffect {
 
     public static void main(String[] args) throws InterruptedException {
-
         String text = "Java - это C++, из которого убрали все пистолеты, ножи и дубинки.\n- James Gosling\n";
         printText(searchWords(text));
         text = "Чтобы написать чистый код, мы сначала пишем грязный код, затем рефакторим, его.\n- Robert Martin\n";
@@ -14,45 +13,37 @@ public class TypewriterStrings {
         printText(searchWords(text));
     }
 
-    public static String[] searchWords(String s) {
-        if (s == null || s.isBlank()) {
+    private static String[] searchWords(String inputText) {
+        if (inputText == null || inputText.isBlank()) {
             System.out.print("Строка не может быть пустой или иметь значение null.\n");
             return new String[0];
         }
-        String[] words = s.split(" ");
+        String[] words = inputText.split(" ");
         String longWord = "";
-        int indexLongWord = 0;
-        String shortWord = s;
-        int indexShortWord = 0;
+        int indexStart = 0;
+        String shortWord = inputText;
+        int indexFinish = 0;
 
         for (int i = 0; i < words.length; i++) {
             words[i] = words[i].replaceAll("[^а-яА-Яa-zA-Z0-9+]", "");
             if (words[i].length() > longWord.length()) {
                 longWord = words[i];
-                indexLongWord = i;
+                indexStart = i;
             }
             if (words[i].length() < shortWord.length()) {
                 if (words[i].isBlank()) continue;
                 shortWord = words[i];
-                indexShortWord = i;
+                indexFinish = i;
             }
         }
-        int max;
-        int min;
-        if (indexShortWord > indexLongWord) {
-            min = indexLongWord;
-            max = indexShortWord;
-        } else {
-            min = indexShortWord;
-            max = indexLongWord;
+        if (indexStart > indexFinish) {
+            int tmp = indexStart;
+            indexStart = indexFinish;
+            indexFinish = tmp;
         }
-
-        String[] finalText = s.split(" ");
-        for (int i = 0; i < words.length; i++) {
-            while (i >= min && i <= max) {
-                finalText[i] = finalText[i].toUpperCase();
-                min++;
-            }
+        String[] finalText = inputText.split(" ");
+        for (int i = indexStart; i <= indexFinish; i++) {
+            finalText[i] = finalText[i].toUpperCase();
         }
         return finalText;
     }
