3f94bba9bab76e342ff926e5ad07a88fb281541f
shuriking <shura.kravchenko.1992@gmail.com>
2024-10-20 22:52:29 +0600

    "Модифицировал Калькулятор"

--- src/com/startjava/lesson_2_3/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3/calculator/Calculator.java
@@ -1,6 +1,10 @@
 package com.startjava.lesson_2_3.calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
+
+    static Scanner scanner = new Scanner(System.in);
     private char sign;
     private int a;
     private int b;
@@ -9,6 +13,35 @@ public class Calculator {
         this.a = a;
         this.b = b;
         this.sign = sign;
+    }
+
+    public static String[] inputExpression() {
+        boolean hasGign = false;
+        String[] arrayExpression;
+        do {
+            String expression = scanner.nextLine();
+            String[] signs = {"+", "-", "*", "/", "^", "%"};
+            arrayExpression = expression.split(" ");
+            for (String s : signs) {
+                if (s.equals(arrayExpression[1])) {
+                    hasGign = true;
+                    break;
+                }
+            }
+            if (!hasGign) {
+                System.out.println("Ошибка: операция " + arrayExpression[1] + " не поддерживается");
+                System.out.println("Доступны следующие операции: +, -, *, /, ^, %");
+
+            }
+        } while (!hasGign);
+        return arrayExpression;
+    }
+
+    public static Calculator changeText(String[] signs) {
+        int x = Integer.parseInt(signs[0]);
+        char y = signs[1].charAt(0);
+        int z = Integer.parseInt(signs[2]);
+        return new Calculator(x, z, y);
     }
 
     public double calculates() {
@@ -20,22 +53,25 @@ public class Calculator {
             case '*':
                 return a * b;
             case '/':
-                return (double) a / b;
+                if (b == 0) {
+                    System.out.println("Деление на ноль запрещено!");
+                    return Double.NaN;
+                } else {
+                    return (double) a / b;
+                }
             case '%':
-                return a % b;
+                if (b == 0) {
+                    System.out.println("Деление на ноль запрещено!");
+                    return Double.NaN;
+                } else {
+                    return Math.floorMod(a, b);
+                }
             case '^':
-                return pow();
+                return Math.pow(a, b);
             default:
                 break;
         }
         return 0;
     }
 
-    private double pow() {
-        double result = 1;
-        for (int i = 1; i <= Math.abs(b); i++) {
-            result *= a;
-        }
-        return b >= 0 ? result : 1 / result;
-    }
 }
--- src/com/startjava/lesson_2_3/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3/calculator/CalculatorTest.java
@@ -1,40 +1,27 @@
 package com.startjava.lesson_2_3.calculator;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String continuation = "yes";
-        while (continuation.equals("yes")) {
-            System.out.println("Введите первое число: ");
-            int a = scanner.nextInt();
-            System.out.println("Введите знак операции (+, -, *, /, ^, %): ");
-            char sign = ' ';
-            do {
-                sign = scanner.next().charAt(0);
-                if (sign == '+' || sign == '-' || sign == '*' || 
-                        sign == '/' || sign == '^' || sign == '%') {
-                    break;
+        while (!continuation.equals("no")) {
+            if (continuation.equals("yes")) {
+                System.out.println("Введите математическое выражение:");
+                String[] expression = Calculator.inputExpression();
+                Calculator calculator = Calculator.changeText(expression);
+                double result = calculator.calculates();
+                if (!Double.isNaN(result)) {
+                    DecimalFormat decimalFormat = new DecimalFormat("#.###");
+                    System.out.println(decimalFormat.format(result));
                 }
-                System.out.println("Ошибка: операция '" + sign + "' не поддерживается");
-                System.out.println("Доступны следующие операции: +, -, *, /, ^, %");
-            } while (true);
-            int b;
-            do {
-                System.out.println("Введите второе число: ");
-                b = scanner.nextInt();
-                if (sign == '/' || sign == '%' && b == 0) {
-                    System.out.println("Ошибка: деление на ноль запрещено");
-                    System.out.println("Введите цифру или число не равное 0");
-                }
-            } while (b == 0);
-            Calculator calculator = new Calculator(a, b, sign);
-            System.out.print(a + " " + sign + " " + b + " = " + calculator.calculates());
-            do {
-                System.out.println("\nХотите продолжить вычисления? [yes/no]:");
-                continuation = scanner.next();
-            } while (!continuation.equals("yes") && !continuation.equals("no")); 
+                System.out.println("Хотите продолжить вычисления? [yes / no]:");
+            } else {
+                System.out.println("Введите корректный ответ [yes / no]:");
+            }
+            continuation = scanner.nextLine();
         }
     }
 }
