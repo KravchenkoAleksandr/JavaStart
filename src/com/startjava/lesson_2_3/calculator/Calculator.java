801279e3a62551dbeca099a4581fdfc27cf26531
Kravchenko Aleksandr <shura.kravchenko.1992@gmail.com>
2024-11-09 00:00:36 +0600

    "Исправил по сообщению от 07.11."

--- src/com/startjava/lesson_2_3/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3/calculator/Calculator.java
@@ -3,37 +3,35 @@ package com.startjava.lesson_2_3.calculator;
 public class Calculator {
 
     public static double calculates(String expression) {
-        String[] example = expression.split(" ");
-        int a = Integer.parseInt(example[0]);
-        char sign = example[1].charAt(0);
-        int b = Integer.parseInt(example[2]);
-        switch (sign) {
-            case '+':
-                return a + b;
-            case '-':
-                return a - b;
-            case '*':
-                return a * b;
-            case '/':
+        String[] elements = expression.split(" ");
+        int a = Integer.parseInt(elements[0]);
+        char sign = elements[1].charAt(0);
+        int b = Integer.parseInt(elements[2]);
+        return switch (sign) {
+            case '+' -> a + b;
+            case '-' -> a - b;
+            case '*' -> a * b;
+            case '/' -> {
                 if (b == 0) {
                     System.out.println("Деление на ноль запрещено!");
-                    return Double.NaN;
+                    yield Double.NaN;
                 } else {
-                    return (double) a / b;
+                    yield (double) a / b;
                 }
-            case '%':
+            }
+            case '%' -> {
                 if (b == 0) {
                     System.out.println("Деление на ноль запрещено!");
-                    return Double.NaN;
+                    yield Double.NaN;
                 } else {
-                    return Math.floorMod(a, b);
+                    yield Math.floorMod(a, b);
                 }
-            case '^':
-                return Math.pow(a, b);
-            default:
-                break;
-        }
-        return 0;
+            }
+            case '^' -> Math.pow(a, b);
+            default -> {
+                System.out.println("Знак " + sign + " не поддерживается");
+                yield Double.NaN;
+            }
+        };
     }
-
 }
