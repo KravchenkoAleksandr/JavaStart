bac55c516c762dd1c342d42c00afa168667d6ed8
Kravchenko Aleksandr <shura.kravchenko.1992@gmail.com>
2024-11-30 21:21:40 +0600

    "Добавил калькулятор с *"

---
+++ src/com/startjava/lesson_2_3_4/calculator/LimitExpression.java
@@ -0,0 +1,8 @@
+package com.startjava.lesson_2_3_4.calculator;
+
+public class LimitExpression extends RuntimeException {
+
+    public LimitExpression(String message) {
+        super(message);
+    }
+}
