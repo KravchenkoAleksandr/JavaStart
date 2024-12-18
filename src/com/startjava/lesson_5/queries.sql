cd710e46763b9ece83b28878f87e1d1691d358a8
Kravchenko Aleksandr <shura.kravchenko.1992@gmail.com>
2024-12-18 13:16:09 +0600

    "Добавил домашнее задание на тему “Базы данных”"

--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -1,27 +1,27 @@
-\echo 'Вывод: всей таблицы';
+\echo 'Р’С‹РІРѕРґ: РІСЃРµР№ С‚Р°Р±Р»РёС†С‹';
 SELECT *
   FROM jaegers
  ORDER BY model_name;
 
-\echo 'Вывод: только не уничтоженных роботов';
+\echo 'Р’С‹РІРѕРґ: С‚РѕР»СЊРєРѕ РЅРµ СѓРЅРёС‡С‚РѕР¶РµРЅРЅС‹С… СЂРѕР±РѕС‚РѕРІ';
 SELECT *
   FROM jaegers
  WHERE status = 'Active'
  ORDER BY model_name;
 
-\echo 'Вывод: только роботов серии Mark-1 и Mark-4';
+\echo 'Р’С‹РІРѕРґ: С‚РѕР»СЊРєРѕ СЂРѕР±РѕС‚РѕРІ СЃРµСЂРёРё Mark-1 Рё Mark-4';
 SELECT *
   FROM jaegers
  WHERE mark IN ('Mark-1', 'Mark-4')
  ORDER BY model_name;
 
-\echo 'Вывод: всех роботов, кроме Mark-1 и Mark-4';
+\echo 'Р’С‹РІРѕРґ: РІСЃРµС… СЂРѕР±РѕС‚РѕРІ, РєСЂРѕРјРµ Mark-1 Рё Mark-4';
 SELECT *
   FROM jaegers
  WHERE mark NOT IN ('Mark-1', 'Mark-4')
  ORDER BY model_name DESC;
 
-\echo 'Вывод: информацию о самых старых роботах';
+\echo 'Р’С‹РІРѕРґ: РёРЅС„РѕСЂРјР°С†РёСЋ Рѕ СЃР°РјС‹С… СЃС‚Р°СЂС‹С… СЂРѕР±РѕС‚Р°С…';
 SELECT *
   FROM jaegers
  WHERE launch = 
@@ -30,7 +30,7 @@ SELECT *
  ORDER BY model_name;
 
 
-\echo 'Вывод: инфу из столбцов model_name, mark, launch, kaiju_kill тех роботов, которые уничтожили больше всех kaiju';
+\echo 'Р’С‹РІРѕРґ: РёРЅС„Сѓ РёР· СЃС‚РѕР»Р±С†РѕРІ model_name, mark, launch, kaiju_kill С‚РµС… СЂРѕР±РѕС‚РѕРІ, РєРѕС‚РѕСЂС‹Рµ СѓРЅРёС‡С‚РѕР¶РёР»Рё Р±РѕР»СЊС€Рµ РІСЃРµС… kaiju';
 SELECT model_name, mark, launch, kaiju_kill
   FROM jaegers
  WHERE kaiju_kill = 
@@ -38,18 +38,18 @@ SELECT model_name, mark, launch, kaiju_kill
  ORDER BY model_name; 
 
 
-\echo 'Вывод: средний вес роботов, округлив его до трех знаков после запятой';
+\echo 'Р’С‹РІРѕРґ: СЃСЂРµРґРЅРёР№ РІРµСЃ СЂРѕР±РѕС‚РѕРІ, РѕРєСЂСѓРіР»РёРІ РµРіРѕ РґРѕ С‚СЂРµС… Р·РЅР°РєРѕРІ РїРѕСЃР»Рµ Р·Р°РїСЏС‚РѕР№';
 SELECT ROUND(AVG(weight)::NUMERIC , 3) 
   FROM jaegers;
 
-\echo 'Вывод: увеличьте на единицу количество уничтоженных kaiju у неразрушенных роботов,а затем отобразите таблицу';
+\echo 'Р’С‹РІРѕРґ: СѓРІРµР»РёС‡СЊС‚Рµ РЅР° РµРґРёРЅРёС†Сѓ РєРѕР»РёС‡РµСЃС‚РІРѕ СѓРЅРёС‡С‚РѕР¶РµРЅРЅС‹С… kaiju Сѓ РЅРµСЂР°Р·СЂСѓС€РµРЅРЅС‹С… СЂРѕР±РѕС‚РѕРІ,Р° Р·Р°С‚РµРј РѕС‚РѕР±СЂР°Р·РёС‚Рµ С‚Р°Р±Р»РёС†Сѓ';
 UPDATE jaegers
    SET kaiju_kill = kaiju_kill + 1
  WHERE status = 'Active';
 SELECT *
   FROM jaegers;
 
-\echo 'Вывод: удалите уничтоженных роботов, а затем отобразите оставшихся';
+\echo 'Р’С‹РІРѕРґ: СѓРґР°Р»РёС‚Рµ СѓРЅРёС‡С‚РѕР¶РµРЅРЅС‹С… СЂРѕР±РѕС‚РѕРІ, Р° Р·Р°С‚РµРј РѕС‚РѕР±СЂР°Р·РёС‚Рµ РѕСЃС‚Р°РІС€РёС…СЃСЏ';
 DELETE 
   FROM jaegers
  WHERE status = 'Destroyed';
